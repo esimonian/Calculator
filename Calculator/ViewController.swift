b66c05f315671d2630467726c5a64daff84fc63e
Elizabeth <eli.simonian@gmail.com>
2016-02-08 00:16:31 -0800

    bug fixes

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,112 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var op: Int = 0
+    var setZero: Bool = true
+    var operand: Int = 0
+    
+    
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func numericPressed(sender: AnyObject) {
+        
+        if setZero {
+            resultLabel.text = "0"
+            setZero = false
+        }
+        
+        var result: Int = Int(resultLabel.text!)!
+        
+        result = result * 10 + sender.tag!
+        resultLabel.text="\(result)"
+        
+    }
+    
+    
+    @IBAction func clearPressed(sender: AnyObject) {
+        resultLabel.text = "0"
+    }
+    
+    
+    @IBAction func equalsPressed(sender: AnyObject) {
+        
+        operand = calculateResult(oprtr: op)
+        resultLabel.text = "\(operand)"
+        op = 0
+        setZero = true
+            
+        
+    }
+    
+    @IBAction func operatorPressed(sender: AnyObject) {
+        
+        if op != 0 {
+            
+            operand = calculateResult(oprtr: op)
+            resultLabel.text = "\(operand)"
+            setZero = true
+            op = sender.tag
+            
+        } else {
+            
+            let res: Int = Int(resultLabel.text!)!
+            operand = res
+            setZero = true
+            op = sender.tag
+            
+        }
+        
+       
+    }
+    
+    func calculateResult(oprtr oprtr: Int) -> Int {
+        
+        var returnValue: Int = Int(resultLabel.text!)!
+        
+        switch(oprtr) {
+            
+        case 1:
+            returnValue = operand * returnValue
+            break
+            
+        case 2:
+            returnValue = operand / returnValue
+            break
+            
+        case 3:
+            returnValue = operand + returnValue
+            break
+            
+        case 4:
+            returnValue = operand - returnValue
+            break
+            
+        default:
+            break
+        }
+        
+        return returnValue
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setZero = true
+        op = 0
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+        
+        
     }
 
 
